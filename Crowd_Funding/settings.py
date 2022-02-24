564606f8331aac35a13d956684c835faa4243600
Masozera <masozeravictor@gmail.com>
2022-02-24 12:33:47 +0300

    ijjj

--- Crowd_Funding/settings.py
+++ Crowd_Funding/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    'crowd_funfing_app'
+
+
+
+
 ]
 
 MIDDLEWARE = [
@@ -123,3 +130,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'crowd_funding_app.CustomUser'
